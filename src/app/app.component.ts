8c9cfa3c55be7b02f031459e0cb2d6704f8178fe
Asmaa-A7med <asmaaahmed02758@gmail.com>
2025-06-01 06:27:02 +0300

    localization

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ManageComponent } from './components/manage/manage/manage.component';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,ManageComponent],
+  imports: [RouterOutlet,ManageComponent,TranslateModule],
   templateUrl: './app.component.html',
+    standalone: true,
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'Emloyee-Front';
+  //title = 'Emloyee-Front';
+   title = 'translation-demo';
 }
